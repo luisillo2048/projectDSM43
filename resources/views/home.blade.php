d5a4e1a4c8fe81d35ae2a64db759a77ec665927b
luisillo2048 <lukewarmluis119@protonmail.com>
2023-12-08 00:58:14 -0600

    Actualicé vista otra vez

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="nuevoLink">ILoveTraveling</a>
+        <a class="navbar-brand" href="#">ILoveTraveling</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
